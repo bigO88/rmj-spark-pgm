abd857ef5331503ab11e76c06e5b6298af4ad80c
Raj janwa <rajjanwa@gmail.com>
2017-08-17 21:58:04 +0100

    Added code for sortbyKey

--- src/main/scala/com/rmj/sorting/SortRunner.scala
+++ src/main/scala/com/rmj/sorting/SortRunner.scala
@@ -1,7 +1,7 @@
 package com.rmj.sorting
 
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkConf
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * Created by rajjanwa on 8/16/17.
   */
@@ -11,12 +11,27 @@ object SortRunner {
 
     println("Sorting application running....")
 
-    val conf = new SparkConf().setAppName("Simple Application").setMaster("local[*]")
+    val conf = new SparkConf().setAppName("Sorting Application").setMaster("local[*]")
     val sc = new SparkContext(conf)
+    val inFilePath = "/home/rajjanwa/input/unsorted_records.csv"
+    val outFilePath = "/home/rajjanwa/output/sorted_records"
+    val rdd = sc.textFile(inFilePath)
+    val employeeRdd = rdd.map { x =>
+      Employee(x.split(",", -1)(0), x.split(",", -1)(1), x.split(",", -1)(2), x.split(",", -1)(3))
+    }
 
-    val rdd=sc.textFile("/home/rajjanwa/ins")
-    rdd.map(x=>x.replaceAll("Raj","Mamta")).saveAsTextFile("/home/rajjanwa/in")
+    val employeeKeyedRdd = employeeRdd.map(x => (x.empId, x))
+
+    val sortedRdd = employeeKeyedRdd.sortByKey()
+
+    val finalRdd = sortedRdd.map { x =>
+      x._2.empId + "|" + x._2.name + "|" + x._2.rank + "|" + x._2.address
+    }
+    finalRdd.saveAsTextFile(outFilePath)
+
   }
+
+  case class Employee(empId: String, name: String, address: String, rank: String)
 
 
 }

6386f85c6e252dcab1a55d4733382d6a0f8171fd
bigO88 <rajjanwa@gmail.com>
2018-06-10 00:54:14 +0530

    updated Docker run script

--- script/docker-run.sh
+++ script/docker-run.sh
@@ -5,7 +5,8 @@ usage()
 {
 echo "Please choose options:"
 echo " 1.) Run cloudera-quickstart docker image with docker-shared directory"
-echo " 2.) Stop cloudera-quickstart docker container"
+echo " 2.) Attach cloudera-quickstart running container"
+echo " 3.) Stop cloudera-quickstart docker container"
 exit 1;
 }
 
@@ -16,7 +17,7 @@ case $1 in
 1)  
 
 echo "Cloudera QuickStart docker starting....."
-docker run --hostname=quickstart.cloudera --privileged=true -d -t -i -p 8888 -p 8080 cloudera/quickstart /usr/bin/docker-quickstart -V /home/rajjanwa/docker-shared
+docker run --hostname=quickstart.cloudera --privileged=true -d -t -i -p 8888 -p 8080 cloudera/quickstart /usr/bin/docker-quickstart -v /home/rajjanwa/docker-shared:/data
 rc=$?
 if [ $rc -eq 0 ]
 then
@@ -27,7 +28,31 @@ fi
 ;;
 
 2)
-docker ps|grep quickstart
+pid=`docker ps|grep quickstart`
+if [ $? -eq 0 ]
+then
+container_id=`docker ps|grep quickstart|awk '{print $1}'`
+docker attach $container_id
+rc=$?
+
+if [ $rc -eq 0 ]
+then
+echo "Cloudera quickstart container attached"
+else
+echo "Cloudera quickstart container failed to attached.Please check"
+fi
+
+
+
+
+else
+echo "No cloudera quickstart container running."
+fi
+
+;;
+
+3)
+pid=`docker ps|grep quickstart`
 if [ $? -eq 0 ]
 then
 container_id=`docker ps|grep quickstart|awk '{print $1}'`
